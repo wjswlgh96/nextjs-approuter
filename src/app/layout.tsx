bd0a693236f562719a73283b0c98e956f0b74d69
JiHo Jeon <wjswlgh96@naver.com>
2024-11-04 13:19:22 +0900

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable}`}
+        suppressHydrationWarning
+      >
         {children}
       </body>
     </html>
